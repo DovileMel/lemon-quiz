f7843a0df1846eaaae3234f0d5410abbccea870f
DovileMel <dovilemelynyte@gmail.com>
2018-07-15 20:56:36 +0300

    meta tag added to the client for responsive layout

--- quiz_app/src/components/App.js
+++ quiz_app/src/components/App.js
@@ -7,6 +7,7 @@ import PropTypes from 'prop-types';
 import Question from './Question';
 import UserDataContainer from './UserDataContainer';
 import ResultContainer from './ResultContainer';
+import MetaTags from 'react-meta-tags';
 
 class App extends React.PureComponent {
 
@@ -18,17 +19,22 @@ class App extends React.PureComponent {
 
   render() {
     return (
-      <div className="content">
-        <header>
-          <h1>Welcome to the Quiz!</h1>
-          <h2>Answer these simple questions and get the results</h2>
-        </header>
-        <main>
-          {this.props.questionnaire && this.props.questionnaire[0].questions.map((quest, index) => <Question question_data={quest} key={index} />)
-          }
-          <UserDataContainer />
-          {this.props.result && <ResultContainer resultData={this.props.result} />}
-        </main>
+      <div className="wrapper">
+        <MetaTags>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </MetaTags>
+        <div className="content">
+          <header>
+            <h1>Welcome to the Quiz!</h1>
+            <h2>Answer these simple questions and get the results</h2>
+          </header>
+          <main>
+            {this.props.questionnaire && this.props.questionnaire[0].questions.map((quest, index) => <Question question_data={quest} key={index} />)
+            }
+            <UserDataContainer />
+            {this.props.result && <ResultContainer resultData={this.props.result} />}
+          </main>
+        </div>
       </div>
     );
   }
